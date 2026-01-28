d973598e9db6c64ae7f71bea9e4f18256a8de886
Diana Usenalieva <dianausenalieva@gmail.com>
2026-01-28 11:11:59 +0500

    type of string

--- practice1/string.py
+++ practice1/string.py
@@ -1,3 +1,18 @@
 #1
 print("Hello")
 print('Hello')
+#2
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+#3
+a = "Hello"
+print(a)
+#4
+b = "Hello, World!"
+print(b[2:5])
+#5
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
